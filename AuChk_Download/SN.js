63d35a660f24889a6d320834b2fed211eeed87ac
iamsqy <iamsqy@outlook.com>
2020-07-06 23:24:59 +0800

    Download func

---
+++ AuChk_Download/SN.js
@@ -0,0 +1,15 @@
+var SNList=new Array()
+SNList[0]="E9eFi2vF-625-028"
+SNList[1]="J8jdpfrE-595-420"
+SNList[2]="N8ndwZoD-905-812"
+SNList[3]="I7i7wRkC-880-217"
+SNList[4]="N7nU8LhA-190-014"
+SNList[5]="R7rsFFwK-165-406"
+SNList[6]="N6nsF8tI-470-798"
+
+
+
+
+
+
+
